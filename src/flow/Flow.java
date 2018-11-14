3a0f3476a2957fbb7f78c05a2f7b7e067e12bcb9
mrfinnessey <MikeFinnessey@MikeFinsseysMBP.hsd1.ca.comcast.net>
2018-11-13 20:33:59 -0800

    Add Sheet Button and numberOfSheets
    Button is currently nonfunctional. Also there is odd stuff with the graphics, most likely due to me not understanding them.

--- src/flow/Flow.java
+++ src/flow/Flow.java
@@ -3,6 +3,7 @@ package flow;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Flow {
@@ -12,8 +13,12 @@ public class Flow {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+		JButton addSheet = new JButton("Add Sheet");
+		addSheet.setPreferredSize(new Dimension (50,50));
+		frame.add(addSheet);
+		addSheet.setLocation((int) (screenSize.getWidth() - 50), (int) (screenSize.getHeight() - 50));
 		frame.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
+		frame.pack();
 		frame.setVisible(true);
 		
 	}
--- src/flow/Sheet.java
+++ src/flow/Sheet.java
@@ -4,9 +4,11 @@ public class Sheet {
 	boolean onCase;
 	String name;
 	int initialSpeech;
+	static int numberOfSheets = 0;
 	public Sheet(boolean onCase, String name, int initialSpeech) {
 		this.onCase = onCase;
 		this.name = name;
 		this.initialSpeech = initialSpeech;
+		numberOfSheets++;
 	}
 }
