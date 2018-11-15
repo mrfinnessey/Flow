9719091cae74c66180a109696ad7e122a602adb1
mrfinnessey <MikeFinnessey@MikeFinsseysMBP.hsd1.ca.comcast.net>
2018-11-14 20:27:44 -0800

    Getter method for each Sheet.
    Current idea is to store the text of each sheet in a string with special characters representing the various "level changes".

--- src/flow/Sheet.java
+++ src/flow/Sheet.java
@@ -4,11 +4,16 @@ public class Sheet {
 	boolean onCase;
 	String name;
 	int initialSpeech;
+	String text = "";
 	static int numberOfSheets = 0;
 	public Sheet(boolean onCase, String name, int initialSpeech) {
 		this.onCase = onCase;
 		this.name = name;
 		this.initialSpeech = initialSpeech;
 		numberOfSheets++;
+	}
+	
+	public String getSheet() {
+		return text;
 	}
 }
